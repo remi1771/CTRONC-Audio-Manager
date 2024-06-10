259bc38f3a4f5848c8db7b9ff08f52dec3345e00
remi1771 <53633938+remi1771@users.noreply.github.com>
2024-06-10 04:49:33 -0300

    Update REPACK.bat so it worksss

--- REPACK.bat
+++ REPACK.bat
@@ -6,9 +6,9 @@ set "HCA_FOLDER=%~dp0HCA"
 set "AWB_FOLDER=%~dp0AWB"
 del /F /S /Q "%HCA_FOLDER%\*.*"
 
-START VGAudioCli.exe -b -i "%WAV_FOLDER%" -o "%HCA_FOLDER%" --out-format hca
+VGAudioCli.exe -b -i "%WAV_FOLDER%" -o "%HCA_FOLDER%" --out-format hca
 
-start /wait "" "quickbms.exe" -w -r -R awb_hca.bms "%AWB_FOLDER%\*.awb" "%HCA_FOLDER%"
+quickbms.exe -G -w -r awb_hca.bms "%AWB_FOLDER%\*.awb" "%HCA_FOLDER%"
 
 echo DONE!?
 PAUSE
